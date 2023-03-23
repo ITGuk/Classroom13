44d03535c3fbffece5d0320283e97fdf5a9a01d8
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-03-23 19:50:07 +0200

    Add files via upload

---
+++ Task5/Class_Task5.cpp
@@ -0,0 +1,50 @@
+//Create two two-dimensional arrays and fill them
+//random values using a function
+//rand().Display matrices on the screen.Multiply the first one
+//square the matrix and output the result in the form
+//matrices.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //task5
+    const int SIZE = 5;
+    int arr[SIZE][SIZE] {};
+    int arr_two[SIZE][SIZE]{};
+    srand(time(NULL));
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            int ran = rand() % 9 + 1;
+            arr[i][j] = ran;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            int ran = rand() % 9 + 1;
+            arr_two[i][j] = ran;
+            cout << arr_two[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            arr_two[i][j] *= arr[i][j];
+            cout << arr_two[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
